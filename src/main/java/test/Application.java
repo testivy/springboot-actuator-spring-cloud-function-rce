1ba41e2ee51ffab8140f44674638e35ffbf9574d
testivy <testivy@github.com>
2022-03-28 14:31:03 +0800

    init

---
+++ src/main/java/test/Application.java
@@ -0,0 +1,71 @@
+package test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.function.context.MessageRoutingCallback;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.util.MultiValueMap;
+import reactor.core.publisher.Flux;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+
+@SpringBootApplication(proxyBeanMethods = false)
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+//    @Bean
+//    public MessageRoutingCallback customRouter() {
+//        return new MessageRoutingCallback() {
+//            @Override
+//            public FunctionRoutingResult routingResult(Message<?> message) {
+//                return new FunctionRoutingResult((String)message.getHeaders().get("Func-Name"));
+//            }
+//        };
+//    }
+
+    @EnableAutoConfiguration
+    public static class RoutingFunctionConfiguration {
+
+        @Bean
+        public Function<String, String> echo() {
+            return x -> {
+                System.out.println("===> echo");
+                return x+" echo function";
+            };
+        }
+
+        @Bean
+        public Function<Person, Person> pojoecho() {
+            return x -> {
+                System.out.println("===> pojoecho");
+                return x;
+            };
+        }
+
+
+        @SuppressWarnings("unused")
+        private static class Person {
+            private String name;
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+        }
+    }
+}
